a1ef4e59acc65fdd58b5b3b22b21cef8c919f07a
Zenov <zeno.vandenbulcke@student.kuleuven.be>
2022-04-07 19:25:49 +0200

    selection sort done

--- selectionsort.py
+++ selectionsort.py
@@ -1,9 +1,25 @@
 def selection_sort(array):
-    pass
+    N = len(array)
+    for i in range(N):
+        minimum = i
+        for j in range(i+1, N):
+            if array[j] < array[minimum]:
+                minimum = j
+        exchange(array, i, minimum)
+    return array
+
+
+def exchange(array, i, j):
+
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
 
 
 def main():
-    pass
+
+    array = [6, 3, 9, 2, 1, 5]
+    sorted_array = selection_sort(array)
 
 
 main()
