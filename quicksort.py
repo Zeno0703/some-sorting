4cb21da48b2cb1a49ae1fb528fb1ae82a483bdfa
Zenov <zeno.vandenbulcke@student.kuleuven.be>
2022-04-09 19:53:57 +0200

    eerste beetje

---
+++ quicksort.py
@@ -0,0 +1,18 @@
+def quicksort(array):
+    N = len(array)
+    pivot = array[N//2]
+    for i in range(N):
+        if array[i] >= pivot:
+            j = N
+            while j
+
+
+def swap(array, i, j):
+    pass
+
+
+def main():
+    pass
+
+
+main()
