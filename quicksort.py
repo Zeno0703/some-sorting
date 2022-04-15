84537853f6faf354ffb70325695fb80c8831b93e
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2022-04-15 18:00:35 +0200

    doesn't work

--- quicksort.py
+++ quicksort.py
@@ -1,19 +1,28 @@
 def quicksort(array):
     N = len(array)
-    pivot = array[N//2]
+    pivot_idx = N // 2
+    pivot = array[pivot_idx]
     for i in range(N):
         if array[i] >= pivot:
-            j = N
-            while j > 0:
-                continue
+            j = N - 1
+            while array[j] > pivot:
+                j -= 1
+            swap(array, i, j)
+        if i == N // 2:
+            break
+    return quicksort(array[:N//2]) + pivot + quicksort(array[N//2 + 1:])
 
 
 def swap(array, i, j):
-    pass
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
 
 
 def main():
-    pass
+    array = [3, 9, 6, 1, 2, 5, 4, 8]
+    sortedArray = quicksort(array)
+    print(sortedArray)
 
 
 main()
