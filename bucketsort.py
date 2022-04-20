3255c35bc93d10fb47b95864665bba65a829bfc2
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2022-04-20 14:42:27 +0200

    very optimised

--- bucketsort.py
+++ bucketsort.py
@@ -1,32 +1,45 @@
 from insetionsort import *
 
 
-def quicksort(array):
+def bucketsort(array):
 
-    N = len(array)
-
-    bucket_array = []
-    for i in range(N):
-        bucket_array.append([])
-
-    for item in array:
-        bucket_array[int(item // 0.10)].append(item)
-
-    for bucket in bucket_array:
-        insertion_sort(bucket)
-
-    counter = 0
-    for bucket in bucket_array:
-        for item in bucket:
-            array[counter] = item
-            counter += 1
+    bucketArray = createBucketArray(len(array))
+    appendItemsInBuckets(array, bucketArray)
+    sortBuckets(bucketArray)
+    restoreSortedItems(array, bucketArray)
 
     return array
 
 
+def createBucketArray(N):
+    bucketArray = []
+    for i in range(N):
+        bucketArray.append([])
+    return bucketArray
+
+
+def appendItemsInBuckets(array, bucketArray):
+    for item in array:
+        bucketArray[int(item // 0.10)].append(item)
+
+
+def sortBuckets(bucketArray):
+    for bucket in bucketArray:
+        insertion_sort(bucket)
+
+
+def restoreSortedItems(array, bucketArray):
+    counter = 0
+    for bucket in bucketArray:
+        for item in bucket:
+            array[counter] = item
+            counter += 1
+
+
 def main():
     array = [0.28, 0.34, 0.65, 0.12, 0.33, 0.88, 0.56, 0.55, 0.44, 0.78, 0.32, 0.37, 0.78, 0.99, 0.12, 0.93]
-    sortedArray = quicksort(array)
+    sortedArray = bucketsort(array)
     print(sortedArray)
+
 
 main()
